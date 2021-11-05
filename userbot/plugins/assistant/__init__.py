d1973335219a7305ab1a7f9149e5156017e546c6
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-05 16:56:15 +0530

    Update __init__.py

--- userbot/plugins/assistant/__init__.py
+++ userbot/plugins/assistant/__init__.py
@@ -5,22 +5,22 @@ from telethon import Button, custom
 from userbot import bot
 
 from userbot import ALIVE_NAME
-OWNER_NAME = ALIVE_NAME
+OWNER_NAME = ALIVE_NAME or "TONY STARK"
 OWNER_ID = bot.uid
 
 
-PYTHON_USER = bot.me.first_name
-Legendl_Mr_Hacker = bot.uid
+me = bot.me.first_name
+hu = bot.uid
 
-python_mention = f"[{PYTHON_USER}](tg://user?id={Legendl_Mr_Hacker})"
-PYTHON_logo = "./userbot/resources/pics/-6163428037589314866_121.jpg"
-PYTHON_logo1 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
-PYTHON_logo2 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
-PYTHON_logo4 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
-PYTHON_logo3 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
-PYTHONversion = "𝚅9.𝙾.8"
+python_mention = f"[{me}](tg://user?id={hu})"
+JARVIS_logo = "./userbot/resources/pics/-6163428037589314866_121.jpg"
+JARVIS_logo1 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
+JARVIS_logo2 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
+JARVIS_logo4 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
+JARVIS_logo3 = "./userbot/resources/pics/-4965507108355287505_121.jpg"
+JARVISversion = "𝚅1.0"
 
-perf = "[ I N PYTHONBOT ]"
+perf = "[ JARVISBOT ]"
 
 
 DEVLIST = [
