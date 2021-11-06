e3e2ae950533c265b5a1e818df4348ae46f37c31
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-06 20:36:17 +0530

    Update hello.py

--- userbot/main_pl/hello.py
+++ userbot/main_pl/hello.py
@@ -1,2 +1,11 @@
+import random
 from JARVIS.utlis import admin_command,sudo_command,edit_or_reply
 
+hello_message = ["Hello","Hi","Hello there","I am here , hello!","Hey"]
+hello_message_random = random.choice(hello_message)
+
+@bot.on(admin_command(pattern = "hi$"))
+@bot.on(sudo_command(patten = "hi$"))
+async def hi(event):
+  id = event.chat_id
+  await bot.send_message(id,hello_message_random)
