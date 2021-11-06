cd7d6c5f44c98330bebc6cd8a18d335a4f220c68
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-06 20:39:58 +0530

    Update hello.py

--- userbot/main_pl/hello.py
+++ userbot/main_pl/hello.py
@@ -1,5 +1,6 @@
 import random
 from JARVIS.utlis import admin_command,sudo_command,edit_or_reply
+from userbot.cmdhelp import CmdHelp
 from . import *
 
 hello_message = ["Hello","Hi","Hello there","I am here , hello!","Hey"]
@@ -10,3 +11,9 @@ hello_message_random = random.choice(hello_message)
 async def hi(event):
   id = event.chat_id
   await bot.send_message(id,hello_message_random)
+
+CmdHelp("hi").add_command(
+    'bot', None, 'Sends a random greeting message'
+).add_type(
+    "Official"
+).add()
