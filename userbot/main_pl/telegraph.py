fae7dedfc3bf23f4be6ca259b92df87b497d16b4
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-12 20:35:58 +0530

    Update telegraph.py

--- userbot/main_pl/telegraph.py
+++ userbot/main_pl/telegraph.py
@@ -50,7 +50,7 @@ async def _(event):
                 end = datetime.datetime.now()
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await eor(event, 
+                await eod(event, 
                    "✓ **[File uploaded to telegraph](https://telegra.ph{})** \n✓ **Time Taken :-** `{}` secs \n✓ **By :- {}** \n✓  `https://telegra.ph{}`".format(
                         media_urls[0], (ms + ms_two), python_mention, media_urls[0],
                     ),
