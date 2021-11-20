2888085965c19f83618d8925059838bb2642e1ed
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-20 09:27:32 +0530

    Update stat.py

--- userbot/main_pl/stat.py
+++ userbot/main_pl/stat.py
@@ -3,12 +3,17 @@ from . import *
 from JARVISBOT.utils import admin_command
 from JARVISBOT.utils import edit_or_reply as eor
 
-group = Config.YOU_GROUP
+group = Config.YOUR_GROUP or "https://t.me/JarvisUserBot_Support"
+group_mention = f"[Group]({group})"
+channel = Config.YOUR_CHANNEL or "https://t.me/Jarvis_Support_Official"
+channel_mention = f"[Channel]({channel})"
 
 stat_message = f"""
-Owner : {jarvis_mention}
+Owner      : {jarvis_mention}
+My Group   : {group_mention}
+My channel : {channel_mention}
 """
-
+"""
 @bot.on(admin_comannd(pattern = ".stat",outgoing = True))
 async def stat(jarvis_op):
-  
+"""
