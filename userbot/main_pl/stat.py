cac7cac741edb12b90e7346db213c46db381bf62
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-20 09:21:47 +0530

    Update stat.py

--- userbot/main_pl/stat.py
+++ userbot/main_pl/stat.py
@@ -1,6 +1,9 @@
+import Config
 from . import *
 from JARVISBOT.utils import admin_command
 from JARVISBOT.utils import edit_or_reply as eor
+
+group = Config.YOU_GROUP
 
 stat_message = f"""
 Owner : {jarvis_mention}
