e12b07059aaa6de55b141c4c15e9ac952ae32624
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-20 09:35:53 +0530

    Update stat.py

--- userbot/main_pl/stat.py
+++ userbot/main_pl/stat.py
@@ -8,12 +8,31 @@ group_mention = f"[Group]({group})"
 channel = Config.YOUR_CHANNEL or "https://t.me/Jarvis_Support_Official"
 channel_mention = f"[Channel]({channel})"
 
+def ping(jarvis_op):
+  if jarvis_op.fwd_from:
+        return
+    start = datetime.now()
+    message = await jarvis_op.eor("STAT")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+
 stat_message = f"""
 Owner      : {jarvis_mention}
 My Group   : {group_mention}
 My channel : {channel_mention}
+Ping       : {ms}
 """
-"""
+
 @bot.on(admin_comannd(pattern = ".stat",outgoing = True))
 async def stat(jarvis_op):
-"""
+  if jarvis_op.fwd_from:
+        return
+    start = datetime.now()
+    message = await jarvis_op.eor("Stats")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await jarvis_op.eor(stat_message)
+
+CmdHelp("stat").add_command("stat", None, "one and only stat command").add_warning(
+    "Harmless Module✅"
+).add_info("Just Like Alive But no pics used").add_type("Official").add()
