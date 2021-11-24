2ea0cdc8538cd42b344d198ece9069d178bb90d2
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-24 17:28:39 +0530

    Update greeting.py

--- userbot/main_pl/greeting.py
+++ userbot/main_pl/greeting.py
@@ -1,13 +1,25 @@
-import random
+"""import random
 from . import *
 from JARVISBOT.utils import admin_cmd,sudo_cmd,eor
 from userbot import ALIVE_NAME
 from userbot.cmdhelp import CmdHelp
 
-#hello_message = ["Hi Guys","Hi!","Hello There","I Am Back",f"{jarvis_mention} arived!"]
-#hello_rand = random.choice(hello_message)
+ts = "Tony Stark"
+jarvis_mention = f"[{DEFAULTUSER or ts}](tg://user?id={TonyStark})"
+
+hello_message = ["Hi Guys","Hi!","Hello There","I Am Back",f"{jarvis_mention} arived!"]
+bye_message = ["Bye","I Am Going!",f"{jarvis_mention} going","Adios"]
+
+hello_rand = random.choice(hello_message)
+bye_rand = random.choice(bye_message)
 
 @bot.on(admin_cmd(pattern = "hi",outgoing = True))
 @bot.on(sudo_cmd(pattern = "hi",allow_sudo = True))
 async def hi(tony):
-  tony.eor()
+  tony.eor(hello_rand)
+
+@bot.on(admin_cmd(pattern = "bye",outgoing = True))
+@bot.on(sudo_cmd(pattern = "bye",allow_sudo= True))
+async def bye(tony):
+  tiny.eor(bye_rand)
+"""
