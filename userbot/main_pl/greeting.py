25c90fa67daedbad46c41fd8bd7ddac1713fe11d
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-24 17:09:50 +0530

    Update greeting.py

--- userbot/main_pl/greeting.py
+++ userbot/main_pl/greeting.py
@@ -1,3 +1,7 @@
-from JARVISBOT.utils import admin_cmd,sudo_cmd,eor
+"""from JARVISBOT.utils import admin_cmd,sudo_cmd,eor
 from userbot import ALIVE_NAME
 from userbot.cmdhelp import CmdHelp
+
+@admin_cmd(pattern = "hi",outgoing = True)
+@sudo_cmd(pattern = "hi",outgoing = )
+"""
