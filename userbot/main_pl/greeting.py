b9ed7acef661a005ef9fc756985eab89dcdd73f1
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-11-24 17:19:06 +0530

    Update greeting.py

--- userbot/main_pl/greeting.py
+++ userbot/main_pl/greeting.py
@@ -1,7 +1,13 @@
-"""from JARVISBOT.utils import admin_cmd,sudo_cmd,eor
+import random
+from . import *
+from JARVISBOT.utils import admin_cmd,sudo_cmd,eor
 from userbot import ALIVE_NAME
 from userbot.cmdhelp import CmdHelp
 
-@admin_cmd(pattern = "hi",outgoing = True)
-@sudo_cmd(pattern = "hi",outgoing = )
-"""
+#hello_message = ["Hi Guys","Hi!","Hello There","I Am Back",f"{jarvis_mention} arived!"]
+#hello_rand = random.choice(hello_message)
+
+@bot.on(admin_cmd(pattern = "hi",outgoing = True))
+@bot.on(sudo_cmd(pattern = "hi",allow_sudo = True))
+async def hi(tony):
+  tony.eor()
