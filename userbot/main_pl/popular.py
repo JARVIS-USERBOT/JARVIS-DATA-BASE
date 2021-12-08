41d07c23d1332a75bb5dcadbae1ef5717d65047d
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-12-08 10:30:11 +0530

    Update popular.py

--- userbot/main_pl/popular.py
+++ userbot/main_pl/popular.py
@@ -20,6 +20,46 @@ async def _(event):
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
         await event.edit(animation_chars[i % 2])
+
+
+@bot.on(admin_cmd(pattern=f"attack$"))
+@bot.on(sudo_cmd(pattern=f"attack$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(0, 14)
+    event = await edit_or_reply(event, "**ATTACK**")
+    animation_chars = [
+    "Attack In Progress",
+    "Missile Launch in: 10",
+    "Missile Launch in: 9",
+    "Missile Launch in: 8",
+    "Missile Launch in: 7",
+    "Missile Launch in: 6",
+    "Missile Launch in: 5",
+    "Missile Launch in: 4",
+    "Missile Launch in: 3",
+    "Missile Launch in: 2",
+    "Missile Launch in: 1",
+    "Missile Launch in: 0",
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully"
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile"
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile \n\nU.S.A Destroyed",
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile \n\nU.S.A Destroyed \n\nChaina Destroyed",
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile \n\nU.S.A Destroyed \n\nChaina Destroyed \n\nEgypt Destroyed",
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile \n\nU.S.A Destroyed \n\nChaina Destroyed \n\nEgypt Destroyed \n\nPakistan Destroyed",
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile \n\nU.S.A Destroyed \n\nChaina Destroyed \n\nEgypt Destroyed \n\nPakistan Destroyed \n\nError",
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile \n\nU.S.A Destroyed \n\nChaina Destroyed \n\nEgypt Destroyed \n\nPakistan Destroyed \n\n_____",
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile \n\nU.S.A Destroyed \n\nChaina Destroyed \n\nEgypt Destroyed \n\nPakistan Destroyed \n\nError",
+    "Missile Launch in: 0 \n\nLaunched Missile Successfully \n\nLaunched 5 Missile \n\nU.S.A Destroyed \n\nChaina Destroyed \n\nEgypt Destroyed \n\nPakistan Destroyed \n\nError \n\nLost Connection"
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 20])
+
 CmdHelp("Popular").add_command(
   "lol", None,"Lol Animation"
-)
+).add_command(
+  "lol", None,"Lol Animation"
+).add
