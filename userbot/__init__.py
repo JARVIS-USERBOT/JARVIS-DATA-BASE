8573e26a8ae603dd61a08343520ca78e41731596
Py1000 <89375743+Py1000@users.noreply.github.com>
2021-12-08 13:41:17 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -215,7 +215,7 @@ SUDO_LIST = {}
 
 JarvisBot = TelegramClient(
     session="Legend-Bot",
-    api_id=Config.APP_ID,
+    api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     connection=ConnectionTcpAbridged,
     auto_reconnect=True,
